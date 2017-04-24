385723e3acfa5e7680430dab719fb2ed4337e8ec
Ricardo Valente <valentemesmo@gmail.com>
2017-04-24 01:31:03 +0100

    More frames on mounth animation

--- WindowsAnimation/Cartolina_Idle_mouth.cs
+++ WindowsAnimation/Cartolina_Idle_mouth.cs
@@ -20,42 +20,58 @@ namespace TriggeredAnimation
 
 {
 	""filename"": ""mounth.swf0000"",
-	""frame"": {""x"":0,""y"":0,""w"":39,""h"":21},
+	""frame"": {""x"":0,""y"":0,""w"":42,""h"":25},
 	""rotated"": false,
 	""trimmed"": false,
-	""spriteSourceSize"": {""x"":0,""y"":0,""w"":39,""h"":21},
-	""sourceSize"": {""w"":39,""h"":21}
+	""spriteSourceSize"": {""x"":0,""y"":0,""w"":42,""h"":25},
+	""sourceSize"": {""w"":42,""h"":25}
 }
 ,{
 	""filename"": ""mounth.swf0001"",
-	""frame"": {""x"":0,""y"":21,""w"":39,""h"":21},
+	""frame"": {""x"":42,""y"":0,""w"":42,""h"":25},
 	""rotated"": false,
 	""trimmed"": false,
-	""spriteSourceSize"": {""x"":0,""y"":0,""w"":39,""h"":21},
-	""sourceSize"": {""w"":39,""h"":21}
+	""spriteSourceSize"": {""x"":0,""y"":0,""w"":42,""h"":25},
+	""sourceSize"": {""w"":42,""h"":25}
 }
 ,{
 	""filename"": ""mounth.swf0002"",
-	""frame"": {""x"":0,""y"":42,""w"":39,""h"":21},
+	""frame"": {""x"":84,""y"":0,""w"":42,""h"":25},
 	""rotated"": false,
 	""trimmed"": false,
-	""spriteSourceSize"": {""x"":0,""y"":0,""w"":39,""h"":21},
-	""sourceSize"": {""w"":39,""h"":21}
+	""spriteSourceSize"": {""x"":0,""y"":0,""w"":42,""h"":25},
+	""sourceSize"": {""w"":42,""h"":25}
 }
 ,{
 	""filename"": ""mounth.swf0003"",
-	""frame"": {""x"":0,""y"":63,""w"":39,""h"":21},
+	""frame"": {""x"":0,""y"":25,""w"":42,""h"":25},
 	""rotated"": false,
 	""trimmed"": false,
-	""spriteSourceSize"": {""x"":0,""y"":0,""w"":39,""h"":21},
-	""sourceSize"": {""w"":39,""h"":21}
+	""spriteSourceSize"": {""x"":0,""y"":0,""w"":42,""h"":25},
+	""sourceSize"": {""w"":42,""h"":25}
+}
+,{
+	""filename"": ""mounth.swf0004"",
+	""frame"": {""x"":42,""y"":25,""w"":42,""h"":25},
+	""rotated"": false,
+	""trimmed"": false,
+	""spriteSourceSize"": {""x"":0,""y"":0,""w"":42,""h"":25},
+	""sourceSize"": {""w"":42,""h"":25}
+}
+,{
+	""filename"": ""mounth.swf0005"",
+	""frame"": {""x"":84,""y"":25,""w"":42,""h"":25},
+	""rotated"": false,
+	""trimmed"": false,
+	""spriteSourceSize"": {""x"":0,""y"":0,""w"":42,""h"":25},
+	""sourceSize"": {""w"":42,""h"":25}
 }],
 ""meta"": {
 	""app"": ""Adobe Flash CS6"",
 	""version"": ""12.0.0.481"",
 	""image"": ""mounth.png"",
 	""format"": ""RGBA8888"",
-	""size"": {""w"":64,""h"":128},
+	""size"": {""w"":128,""h"":128},
 	""scale"": ""1""
 }
 }
